a1085d26d1ea710a170190de95d9a94513e84554
luisisraelVA <luisillo5938@gmail.com>
2026-05-30 20:18:36 -0400

    fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,9 @@
-const CACHE_NAME = 'pocket-lender-v1';
-const urlsToCache = ['/', '/index.html'];
+const CACHE_NAME = 'pocket-lender-v2';
+const urlsToCache = [
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/manifest.json'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -8,6 +12,10 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // No cachear el index.html para evitar conflictos con nuevos deploys
+  if (event.request.url.includes('index.html') || event.request.mode === 'navigate') {
+    return fetch(event.request);
+  }
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
